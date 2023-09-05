ccd7dcbe6fdf25cb713998027adb0cc3b186a9fc
mebefore <825318@naver.com>
2023-09-05 10:13:10 +0900

    chat add

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,29 +1,32 @@
 package com.example.demo;
 
+import java.util.Date;
 import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.chat.Greeting;
 import com.example.demo.customer.Customer;
 import com.example.demo.customer.CustomerRepository;
 
 @RestController
 public class HomeController {
-
 	
-	@Autowired CustomerRepository repo;
+	@Autowired 
+	CustomerRepository repo;
 	
-	@GetMapping("/")
-	public String greet() {
-		return "hello";
-		
-	}
+	@Autowired
+	private SimpMessagingTemplate template;
 	
 	@GetMapping("/cust")
 	public Iterator<Customer> cust(){
+		 String text = "[" + new Date() + "]:" + "cust select";
+	        this.template.convertAndSend("/topic/cust", text);
 		return repo.findAll().iterator();
-		
 	}
+	
+	
 }
---
+++ demo/src/main/java/com/example/demo/chat/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+	 @Override
+	  public void configureMessageBroker(MessageBrokerRegistry config) {
+	    config.enableSimpleBroker("/topic"); //구독 신청 (수신)
+	    config.setApplicationDestinationPrefixes("/app"); //소켓 메세지 전송 (발신)
+	  }
+
+	  @Override
+	  public void registerStompEndpoints(StompEndpointRegistry registry) {
+	    registry.addEndpoint("/chatserver"); //소켓 서버 연결
+	  }
+}
